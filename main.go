94a18d8a1c591dfb6a3470d664d403c187ec8974
Meghan Denny <nektromc@gmail.com>
2019-11-10 03:13:28 -0500

    add config type and var

--- main.go
+++ main.go
@@ -3,10 +3,23 @@ package main
 import (
 	"log"
 
+	oauth2 "github.com/nektro/go.oauth2"
+
 	. "github.com/nektro/go-util/alias"
 
 	_ "github.com/nektro/mantle/statik"
 )
+
+var (
+	config      *Config
+)
+
+type Config struct {
+	Version   int               `json:"version"`
+	Port      int               `json:"port"`
+	Clients   []oauth2.AppConf  `json:"clients"`
+	Providers []oauth2.Provider `json:"providers"`
+}
 
 func main() {
 	log.Println("Welcome to " + Name + ".")

07377ea50cef1b70f9a1446edf06a4de7a9fb1e5
Meghan Denny <nektromc@gmail.com>
2019-11-10 06:00:10 -0500

    add apiBootstrapRequireLogin() and writeAPIResponse()

--- util.go
+++ util.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"strings"
 
+	"github.com/gorilla/sessions"
 	etc "github.com/nektro/go.etc"
 	uuid "github.com/satori/go.uuid"
 
@@ -41,4 +43,41 @@ func assertChannelMessagesTableExists(uid string) {
 		{"text", "text"},
 		{"test", "text"},
 	})
+}
+
+func apiBootstrapRequireLogin(r *http.Request, w http.ResponseWriter, method string, assertMembership bool) (*sessions.Session, *RowUser, error) {
+	if r.Method != method {
+		return nil, nil, writeAPIResponse(r, w, false, http.StatusMethodNotAllowed, "This action requires using HTTP "+method)
+	}
+	if method == http.MethodPost {
+		err := r.ParseForm()
+		if err != nil {
+			return nil, nil, writeAPIResponse(r, w, false, http.StatusBadRequest, "Error parsing form data")
+		}
+	}
+
+	sess := etc.GetSession(r)
+	sessID := sess.Values["user"]
+
+	if sessID == nil {
+		return nil, nil, writeAPIResponse(r, w, false, http.StatusUnauthorized, "Must login to access this resource")
+	}
+
+	userID := sessID.(string)
+	user, _ := queryUserByUUID(userID)
+
+	if assertMembership && !user.IsMember {
+		return nil, nil, writeAPIResponse(r, w, false, http.StatusForbidden, "This action requires being a member of this server. ("+userID+")")
+	}
+
+	return sess, &user, nil
+}
+
+func writeAPIResponse(r *http.Request, w http.ResponseWriter, good bool, status int, message interface{}) error {
+	resp := APIResponse{good, message}
+	dat, _ := json.Marshal(resp)
+	w.WriteHeader(status)
+	w.Header().Add("content-type", "application/json")
+	w.Write(dat)
+	return nil
 }

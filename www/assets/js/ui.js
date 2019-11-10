9fc3677cc3992e33756d3f813015d82c26927a42
Meghan Denny <nektromc@gmail.com>
2019-11-10 06:46:20 -0500

    www/chat/js - add ui and util .js

---
+++ www/assets/js/ui.js
@@ -0,0 +1,46 @@
+/**
+ */
+//
+import { el_1, el_2, el_3, create_element, dcTN, messageCache, output, getUserFromUUID } from "./util.js";
+
+//
+
+export const volatile = {
+    activeChannel: null,
+};
+
+//
+
+export function addChannel(uuid, name) {
+    el_1.firstElementChild.appendChild(create_element("li", [["data-uuid",uuid]], [dcTN(name)]))
+    messageCache.set(uuid, []);
+}
+
+export async function addMessage(channel=volatile.activeChannel.dataset.uuid, from, message, raw_from=false, save=true) {
+    const at_bottom = output.scrollTop === output.scrollTopMax;
+    const uuid = raw_from ? "" : from.uuid;
+    const name = from.nickname || from.name;
+    if (raw_from || output.dataset.active === channel) {
+        output.appendChild(create_element("div", [], [
+            create_element("div", [], [dcTN(name + ": ")]),
+            create_element("div", [], [dcTN(message)])
+        ]));
+    }
+    if (at_bottom) output.scrollTop = output.scrollHeight;
+    if (save===true) messageCache.get(channel).push([uuid, message]);
+}
+
+export async function setActiveChannel(uid) {
+    console.debug("channel-switch:", uid);
+    let ac = el_1.querySelector(".active");
+    if (ac !== null) ac.classList.remove("active");
+    volatile.activeChannel = el_1.querySelector(`[data-uuid="${uid}"]`);
+    volatile.activeChannel.classList.add("active");
+    //
+    output.dataset.active = uid;
+    output.removeAllChildren();
+    const new_message_history = messageCache.get(uid);
+    for (const item of new_message_history) {
+        addMessage(uid, await getUserFromUUID(item[0]), item[1], false, false);
+    }
+}
--- www/assets/js/util.js
+++ www/assets/js/util.js
@@ -7,3 +7,33 @@ export const messageCache = new Map();
 export const el_1 = document.getElementById("channel-list");
 export const el_2 = document.getElementById("server-name");
 export const el_3 = document.getElementById("me");
+
+//
+
+export function create_element(name, attrs, children) {
+    var ele = document.createElement(name);
+    (attrs || []).forEach(function(v) { ele.setAttribute(v[0], v[1]); });
+    (children || []).forEach(function(v) { ele.appendChild(v); });
+    return ele;
+}
+
+export function dcTN(string) {
+    return document.createTextNode(string);
+}
+
+export async function getUserFromUUID(uuid) {
+    if (userCache.has(uuid)) {
+        return userCache.get(uuid);
+    }
+    else {
+        const req = await fetch(`/api/users/${uuid}`);
+        const res = await req.json();
+        if (res.success) {
+            userCache.set(uuid, res.message);
+            return res.message;
+        }
+        else {
+            return null;
+        }
+    }
+}

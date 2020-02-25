da6bbd9cf5cfb26e6eefb2f47a5bfd1d2108772e
Meghan Denny <nektromc@gmail.com>
2020-02-25 01:51:46 -0500

    db: add channel field to message type

--- pkg/db/message.go
+++ pkg/db/message.go
@@ -5,5 +5,6 @@ type Message struct {
 	UUID string `json:"uuid" sqlite:"text"`
 	At   string `json:"time" sqlite:"text"`
 	By   string `json:"author" sqlite:"text"`
+	In   string `json:"channel" sqlite:"text"`
 	Body string `json:"body" sqlite:"text"`
 }

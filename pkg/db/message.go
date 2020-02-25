ac55c56daa4b6fcdfc0983d308f99768207d1875
Meghan Denny <nektromc@gmail.com>
2020-02-25 01:51:27 -0500

    db: remove sent from message fields

--- pkg/db/message.go
+++ pkg/db/message.go
@@ -1,9 +1,9 @@
 package db
 
 type Message struct {
-	ID     int64  `json:"id"`
-	UUID   string `json:"uuid" sqlite:"text"`
-	SentAt string `json:"sent_at" sqlite:"text"`
-	SentBy string `json:"sent_by" sqlite:"text"`
-	Body   string `json:"body" sqlite:"text"`
+	ID   int64  `json:"id"`
+	UUID string `json:"uuid" sqlite:"text"`
+	At   string `json:"time" sqlite:"text"`
+	By   string `json:"author" sqlite:"text"`
+	Body string `json:"body" sqlite:"text"`
 }

26da331d9af7a0dac63e4b45603b283491eeae77
Meghan Denny <nektromc@gmail.com>
2020-02-27 14:46:06 -0500

    db: add ChannelPerms.Scan

--- pkg/db/channel_perms.go
+++ pkg/db/channel_perms.go
@@ -1,8 +1,19 @@
 package db
 
+import (
+	"database/sql"
+
+	dbstorage "github.com/nektro/go.dbstorage"
+)
+
 type ChannelPerms struct {
 	ID        int64  `json:"id"`
 	Channel   string `json:"channel" sqlite:"text"`
 	Type      int    `json:"p_type" sqlite:"int"`
 	Snowflake string `json:"snowflake" sqlite:"text"`
+}
+
+func (v ChannelPerms) Scan(rows *sql.Rows) dbstorage.Scannable {
+	rows.Scan(&v.ID, &v.Channel, &v.Type, &v.Snowflake)
+	return &v
 }

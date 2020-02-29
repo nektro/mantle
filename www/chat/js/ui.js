895014ce0ca2d8eaf60747807b52f248aaa0fc96
Meghan Denny <nektromc@gmail.com>
2020-02-29 03:12:42 -0500

    www- 0-pad 24 time by using en-GB

--- www/chat/js/ui.js
+++ www/chat/js/ui.js
@@ -33,7 +33,7 @@ export function createMessage(user, msg) {
     if (msg.uuid) attrs.push(["data-msg-uid",msg.uuid]);
     if (user.uuid) attrs.push(["data-user-uid",user.uuid]);
     //
-    const time = new Date(msg.time).toLocaleString();
+    const time = new Date(msg.time).toLocaleString("en-GB");
     const el = create_element("div", attrs, [
         create_element("div", [["class","ts"],["title",time]], [dcTN(time.substring(time.indexOf(" ")))]),
         create_element("div", [["class","usr"]], [dcTN(user.name + ": ")]),

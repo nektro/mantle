e1ea17f6f47d3cc7d2fd842925d284fde11423b4
Meghan Denny <nektromc@gmail.com>
2020-02-29 03:02:18 -0500

    handler: make rolesread not require user login

--- pkg/handler/roles.go
+++ pkg/handler/roles.go
@@ -8,9 +8,5 @@ import (
 
 // RolesRead reads info about channel
 func RolesRead(w http.ResponseWriter, r *http.Request) {
-	_, _, err := apiBootstrapRequireLogin(r, w, http.MethodGet, true)
-	if err != nil {
-		return
-	}
 	writeAPIResponse(r, w, true, http.StatusOK, ws.RoleCache)
 }

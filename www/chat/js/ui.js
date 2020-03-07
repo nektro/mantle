d136237b5b4ab9d168acee0bbb9d8f52e7bcbae5
Meghan Denny <nektromc@gmail.com>
2020-03-07 05:32:29 -0500

    www/js- set user name color based on top role

--- www/chat/js/ui.js
+++ www/chat/js/ui.js
@@ -35,6 +35,15 @@ export function createMessage(user, msg) {
     const attrsU = [["class","usr"]];
     if (msg.uuid) attrs.push(["data-msg-uid",msg.uuid]);
     if (user.uuid) attrs.push(["data-user-uid",user.uuid]);
+    if (user.roles) {
+        const a = user.roles.split(",").
+            map((v) => roleCache.get(v)).
+            sort((b,c) => b.position > c.position).
+            filter((v) => v.color.length > 0);
+        if (a.length > 0) {
+            attrsU.push(["data-role",a[0].uuid]);
+        }
+    }
     //
     const time = new Date(msg.time||Date.now()).toLocaleString("en-GB");
     const el = create_element("div", attrs, [

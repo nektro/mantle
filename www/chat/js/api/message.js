3afe087ffd162a865bb71eb1c3f35da69972bb8b
Meghan Denny <nektromc@gmail.com>
2020-04-05 17:01:22 -0400

    www/js/api- make message expect channel uid not object

--- www/chat/js/api/message.js
+++ www/chat/js/api/message.js
@@ -6,13 +6,13 @@ export const cache = new Map();
 
 //
 export class Message {
-    constructor(o, ch) {
+    constructor(o, ch_uid) {
         if (o === null) {
             this.is_null = true;
             return;
         }
         Object.assign(this, o);
         this.is_null = false;
-        cache.get(ch.uuid).set(this.uuid, this);
+        cache.get(ch_uid).set(this.uuid, this);
     }
 }

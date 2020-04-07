3cc0aec6fd29282ad096da34a77f2083ce54775c
Meghan Denny <nektromc@gmail.com>
2020-04-07 14:46:26 -0400

    remove tests from start.sh

--- start.sh
+++ start.sh
@@ -2,13 +2,6 @@
 
 set -e
 set -x
-shopt -s globstar
-
-./node_modules/.bin/jshint ./www/chat/js/**/*.js
-./node_modules/.bin/eslint ./www/chat/js/**/*.js
-
-go test
-go vet -composites=false
 
 go build
 ./mantle \

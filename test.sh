615ec1a0422cbd82e12f14e9d0573985850247c7
Meghan Denny <nektromc@gmail.com>
2020-04-07 14:43:55 -0400

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+shopt -s globstar
+
+
+########################
+#    Golang Testing    #
+########################
+
+# https://golang.org/pkg/testing/
+go test
+
+
+########################
+#  Javascript Testing  #
+########################
+
+# https://jshint.com/
+./node_modules/.bin/jshint ./www/chat/js/**/*.js ./tests/**/*.js
+
+# https://eslint.org/
+./node_modules/.bin/eslint ./www/chat/js/**/*.js ./tests/**/*.js

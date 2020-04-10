16e85c7d23b7b4acb879c28e8a8fbdf144ac2b92
Meghan Denny <nektromc@gmail.com>
2020-04-09 23:21:56 -0400

    www/js- add back unread notification bubbles

--- www/chat/js/x/x-messages.js
+++ www/chat/js/x/x-messages.js
@@ -1,6 +1,7 @@
 "use strict";
 //
 import { create_element } from "./../util.js";
+import { Channel } from "./../ui.channel.js";
 
 //
 customElements.define("x-messages", class extends HTMLElement {
@@ -8,14 +9,24 @@ customElements.define("x-messages", class extends HTMLElement {
         super();
     }
     setActiveChannel(uid) {
+        new Channel(uid).unread = 0;
         this.removeAllChildren();
         this.appendChild(create_element("x-msg-pane", [["uuid",uid]]));
     }
     get active_channel_uid() {
         return this.children[0]._uid;
     }
+    /**
+     * @param {string} ch_uid
+     * @param {api.User} user
+     * @param {api.Message} msg
+     */
     async addMessage(ch_uid, user, msg) {
-        if (this.active_channel_uid !== ch_uid) { return; }
+        const ch_sb = new Channel(ch_uid);
+        if (this.active_channel_uid !== ch_uid) {
+            ch_sb.unread++;
+            return;
+        }
         await this.children[0].appendMessage(user, msg);
     }
     getChannel(uid) {

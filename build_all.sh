4355a10f6e3d56e7ebe8bdc12afc02ae2fe18b7d
Meghan Denny <nektromc@gmail.com>
2020-04-25 03:25:24 -0400

    build_all- static folder is www now that bin_www is gone

--- build_all.sh
+++ build_all.sh
@@ -2,7 +2,7 @@
 
 init() {
     go get -v -u github.com/rakyll/statik
-    $GOPATH/bin/statik -src="./bin_www/"
+    $GOPATH/bin/statik -src="./www/"
 }
 build_template() {
     export CGO_ENABLED=1

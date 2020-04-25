347871e615d7bde0d2739c7f57536e9db75123eb
Meghan Denny <nektromc@gmail.com>
2020-04-25 03:31:48 -0400

    www/js- x-button- add confirmation dialog

--- www/chat/js/x/x-button.js
+++ www/chat/js/x/x-button.js
@@ -12,13 +12,23 @@ customElements.define("x-button", class extends WSetting {
         this.appendChild(create_element("form", null, [
             create_element("button", null, [dcTN(this.getAttribute("text"))]),
         ]));
-        this.querySelector("form").addEventListener("submit", (ev) => {
+        this.querySelector("form").addEventListener("submit", async (ev) => {
             ev.preventDefault();
             const de = this.defaultEndpoint();
             const e = this.getAttribute("endpoint")||de;
             const m = this.getAttribute("method")||"post";
             const f = this.getAttribute("fill")||"";
             const e2 = e.replace("%s", f);
+            if (m === "delete") {
+                const result = await Swal.fire({
+                    title: "Are you sure?",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "Yes, delete it!"
+                });
+                if (!result.value) { return; }
+            }
             return fetch(e2, { method:m, body:new FormData() });
         });
     }

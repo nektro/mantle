3155878e57e36ea00c94980c96f3b6a7dc615d1f
Meghan Denny <nektromc@gmail.com>
2020-06-15 04:22:20 -0400

    ws: add function doc comments

--- pkg/ws/ws.go
+++ pkg/ws/ws.go
@@ -15,9 +15,11 @@ var (
 )
 
 var (
+	// UserCache is the list of users currently with ws connections to this instance
 	UserCache = map[string]*User{}
 )
 
+// Connect takes a db.User and upgrades it to a ws.User
 func Connect(user *db.User, w http.ResponseWriter, r *http.Request) (*User, error) {
 	conn, err := reqUpgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -40,23 +42,26 @@ func Connect(user *db.User, w http.ResponseWriter, r *http.Request) (*User, erro
 	return u, nil
 }
 
+// Close disconnect all remaining users
 func Close() {
-	// disconnect all remaining users
 	for _, item := range UserCache {
 		item.Conn.Close()
 	}
 }
 
+// BroadcastMessage sends message to all users
 func BroadcastMessage(message map[string]interface{}) {
 	for _, item := range UserCache {
 		item.SendMessageRaw(message)
 	}
 }
 
+// AllOnlineIDs returns ULID of every online user
 func AllOnlineIDs() []string {
 	return listToArray(connected)
 }
 
+// OnlineUserCount is the total number of active users
 func OnlineUserCount() int64 {
 	return int64(len(UserCache))
 }

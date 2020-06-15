3156047b2f6a373800c485071b965f9c46fe82ef
Meghan Denny <nektromc@gmail.com>
2020-06-15 04:21:41 -0400

    db: update properties to use store

--- pkg/db/properties.go
+++ pkg/db/properties.go
@@ -2,12 +2,13 @@ package db
 
 import (
 	"strconv"
-	"sync"
+
+	"github.com/nektro/mantle/pkg/store"
 )
 
 // Properties is a KV-store abstraction around a cached db table
 type Properties struct {
-	cache sync.Map
+	s *store.Store
 }
 
 // SetDefault sets a key's value only if that key has never been set
@@ -22,42 +23,35 @@ func (p *Properties) SetDefault(key string, value string) {
 
 // Init is called at all SetDefaults and loads cache from db table
 func (p *Properties) Init() {
-	p.cache = sync.Map{}
-	for _, item := range (Setting{}.All()) {
-		p.cache.Store(item.Key, item.Value)
+	p.s = store.This
+	if !p.s.Has("loaded") {
+		for _, item := range (Setting{}.All()) {
+			p.s.Set(item.Key, item.Value)
+		}
+		p.s.Set("loaded", "1")
 	}
 }
 
 // GetAll returns entire store in a map structure
 func (p *Properties) GetAll() map[string]string {
-	res := map[string]string{}
-	p.cache.Range(func(key interface{}, val interface{}) bool {
-		res[key.(string)] = val.(string)
-		return true
-	})
-	return res
+	return p.GetSome(p.s.Keys()...)
 }
 
 // Get retrieves the value of a single key
 func (p *Properties) Get(key string) string {
-	val, ok := p.cache.Load(key)
-	if !ok {
-		return ""
-	}
-	return val.(string)
+	return p.s.Get(key)
 }
 
 // Set sets the value of a single key
 func (p *Properties) Set(key string, val string) {
 	p.SetDefault(key, "")
 	db.Build().Up(cTableSettings, "value", val).Wh("key", key).Exe()
-	p.cache.Store(key, val)
+	p.s.Set(key, val)
 }
 
 // Has tests whether this Properties contains a certain key
 func (p *Properties) Has(key string) bool {
-	_, ok := p.cache.Load(key)
-	return ok
+	return p.s.Has(key)
 }
 
 // GetSome returns a subset of the store in a map structure

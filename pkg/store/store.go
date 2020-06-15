0c4ebfb43b0c56840f0bb64ace8eea2b13e22304
Meghan Denny <nektromc@gmail.com>
2020-06-15 04:19:52 -0400

    store: put store type in main file

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -1,6 +1,8 @@
 package store
 
 import (
+	"sync"
+
 	"github.com/nektro/mantle/pkg/store/local"
 )
 
@@ -15,4 +17,34 @@ func PreInit() {
 
 // Init takes flag values and initializes datastore
 	This = &Store{local.Get()}
+}
+
+// Inner holds KV values
+type Inner interface {
+	Type() string
+	Has(key string) bool
+	Set(key string, val string)
+	Get(key string) string
+	Range(f func(key string, val string) bool)
+	HasList(key string) bool
+	ListHas(key, value string) bool
+	ListAdd(key, value string)
+	ListRemove(key, value string)
+	ListLen(key string) int
+	sync.Locker
+}
+
+// Store is
+type Store struct {
+	Inner
+}
+
+// Keys returns array of all Keys
+func (p Store) Keys() []string {
+	res := []string{}
+	p.Range(func(k, _ string) bool {
+		res = append(res, k)
+		return true
+	})
+	return res
 }

c3f68a89135638b7bf3c24cfd34973afdedaf69f
Meghan Denny <hello@nektro.net>
2022-04-16 17:34:32 -0700

    make_release- remove iife

--- make_release.sh
+++ make_release.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
-init() {
-    date=$(date +'%Y%m%d')
-    version=${CIRCLE_BUILD_NUM-$date}
-    tag=v$version
-    go get -v -u github.com/tcnksm/ghr
-    $GOPATH/bin/ghr \
-        -t ${GITHUB_TOKEN} \
-        -u ${CIRCLE_PROJECT_USERNAME} \
-        -r ${CIRCLE_PROJECT_REPONAME} \
-        -b "$(./changelog.sh)" \
-        "$tag" \
-        "./bin/"
-}
-
-init
+date=$(date +'%Y%m%d')
+version=${CIRCLE_BUILD_NUM-$date}
+tag=v$version
+go get -v -u github.com/tcnksm/ghr
+$GOPATH/bin/ghr \
+    -t ${GITHUB_TOKEN} \
+    -u ${CIRCLE_PROJECT_USERNAME} \
+    -r ${CIRCLE_PROJECT_REPONAME} \
+    -b "$(./changelog.sh)" \
+    "$tag" \
+    "./bin/"
